b156f97d684529f637a56d1a80f96004a97a7ba9
Tobin Idaerefaa <tidaerefaa@axxess.com>
2023-08-08 20:44:11 +0100

    latest

---
+++ Models/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MotelManagement.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public int Status { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MotelManagement.Models
+{
+    public class Booking:BaseEntity
+    {
+        public string ClientId { get; set; }
+        public int RoomId { get; set; }
+        public int InvoiceId { get; set; }
+        public int Occupants { get; set; }
+    }
+}
